ecbe5a8a34e5e6454bea27a0b0311f220b6b311d
ug-devops <ughani98@gmail.com>
2025-05-06 09:34:30 +0000

    Third commit

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello from Helm!"));
+app.get("/", (req, res) => res.send("Hello from Helm! - This app is develop by Usman Saleem!!!"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
